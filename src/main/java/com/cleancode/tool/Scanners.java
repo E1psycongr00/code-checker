8253fe7889d07dbeb672d18ce48f647679aeb1e1
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-27 22:31:11 +0900

    feat(scanners): add logic to extract metadata

---
+++ src/main/java/com/cleancode/tool/Scanners.java
@@ -0,0 +1,60 @@
+package com.cleancode.tool;
+
+import com.cleancode.util.FileScanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * The scanner is an enum that extracts the necessary reflection information.
+ * Extract constructors, methods, fields, and parameters information
+ * from the extracted class and provide it in {@link Store} format.
+ */
+public enum Scanners {
+    CONSTRUCTOR {
+        @Override
+        void scan(String basePackageName, List<Store<Object>> list) {
+            List<Class<?>> classes = fileScanner.scan(basePackageName);
+            classes.forEach(clazz -> list.add(Store.of(clazz.getName(), clazz.getDeclaredConstructors())));
+        }
+    },
+    METHODS {
+        @Override
+        void scan(String basePackageName, List<Store<Object>> list) {
+            List<Class<?>> classes = fileScanner.scan(basePackageName);
+            classes.forEach(clazz -> list.add(Store.of(clazz.getName(), clazz.getDeclaredMethods())));
+        }
+    },
+    FIELDS {
+        @Override
+        void scan(String basePackageName, List<Store<Object>> list) {
+            List<Class<?>> classes = fileScanner.scan(basePackageName);
+            classes.forEach(clazz -> list.add(Store.of(clazz.getName(), clazz.getDeclaredFields())));
+        }
+    },
+    @Deprecated
+    PARAMETERS {
+        @Override
+        void scan(String basePackageName, List<Store<Object>> list) {
+            List<Class<?>> classes = fileScanner.scan(basePackageName);
+            for (Class<?> clazz : classes) {
+                Stream.of(clazz.getDeclaredMethods()).forEach(
+                        method -> list.add(Store.of(clazz.getName(), method.getParameters())));
+            }
+        }
+    };
+
+    private static final FileScanner fileScanner = new FileScanner(true);
+
+    abstract void scan(String basePackageName, List<Store<Object>> list);
+
+    public List<Store<Object>> scan(String basePackageName) {
+        List<Store<Object>> stores = new ArrayList<>();
+        scan(basePackageName, stores);
+        return stores;
+    }
+
+    public void setRecursive(boolean isRecursive) {
+        fileScanner.setRecursive(isRecursive);
+    }
+}
