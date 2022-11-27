c76776eece2ed9e23cbc4d005f8e383e70edb703
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-28 00:41:29 +0900

    feat(getterSetterChecker): add one class check logic

--- src/main/java/com/cleancode/tool/GetterSetterChecker.java
+++ src/main/java/com/cleancode/tool/GetterSetterChecker.java
@@ -2,7 +2,11 @@ package com.cleancode.tool;
 
 public interface GetterSetterChecker {
 
+    boolean checkNotUsingGetterMethod(Class<?> clazz);
+
     boolean checkNotUsingGetterMethod(String basePackageName, boolean isRecursive);
+
+    boolean checkNotUsingSetterMethod(Class<?> clazz);
 
     boolean checkNotUsingSetterMethod(String basePackageName, boolean isRecursive);
 }
